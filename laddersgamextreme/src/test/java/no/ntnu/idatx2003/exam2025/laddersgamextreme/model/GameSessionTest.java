a988e73fdca6d3e34da2ffe742495d6df028783b
Ink-Song <sunruh@proton.me>
2025-02-26 10:08:47 +0100

    SpecialTile: Added tests for special tile functionality (move pieces)

--- laddersgamextreme/src/test/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/GameSessionTest.java
+++ laddersgamextreme/src/test/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/GameSessionTest.java
@@ -29,9 +29,41 @@ class GameSessionTest {
 
   @Test
   void removePlayer() {
+    //Add two players to the game to see if we can then remove them.
+    Player Dave = new Player("Dave",200);
+    Player player2 = new Player("Julian",75);
+    gameSession.addPlayer(Dave, Color.RED);
+    gameSession.addPlayer(player2, Color.BLUE);
+
+    //Get list of players.
+    List<PlayingPiece> playingPieces = gameSession.getPlayerPieces();
+    assertEquals(2, playingPieces.size());
+
+    //Remove a player
+    gameSession.removePlayer(player2);
+    assertEquals(1, playingPieces.size());
   }
 
   @Test
   void moveCurrentPlayer() {
+    Player Dave = new Player("Dave",200);
+    Player player2 = new Player("Julian",75);
+    gameSession.addPlayer(Dave, Color.RED);
+    gameSession.addPlayer(player2, Color.BLUE);
+
+    //Check to see players current positions
+    gameSession.moveCurrentPlayer(5);
+
+    List<PlayingPiece> playingPieces = gameSession.getPlayerPieces();
+    System.out.println("Printing Player piece positions.");
+    for (PlayingPiece piece : playingPieces) {
+      System.out.println(piece.getPlayer().getPlayerName() + "'s Piece, current tile: " + piece.getCurrentTile());
+    }
+    assertEquals(5,playingPieces.getFirst().getCurrentTile());
+    gameSession.moveCurrentPlayer(10);
+    for (PlayingPiece piece : playingPieces) {
+      System.out.println(piece.getPlayer().getPlayerName() + "'s Piece, current tile: " + piece.getCurrentTile());
+    }
+    assertEquals(10,playingPieces.get(1).getCurrentTile());
   }
 }
