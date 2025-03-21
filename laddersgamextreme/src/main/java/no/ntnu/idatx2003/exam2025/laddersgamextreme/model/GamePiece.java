6bc18f3c26599a81787919ae7a1f5d3ce769a8d8
Ink-Song <sunruh@proton.me>
2025-03-21 12:40:19 +0100

    Added Tile position to Tile, added Tile Action to game Piece.

--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/GamePiece.java
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/GamePiece.java
@@ -1,4 +1,16 @@
 package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
 
 public class GamePiece {
+  private Tile currentTile;
+
+  public Tile getTile(){
+    return currentTile;
+  }
+  public void setTile(Tile tile){
+    currentTile = tile;
+  }
+
+  private void performTileAction(){
+    currentTile.landingAction(this);
+  }
 }
--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Tile.java
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Tile.java
@@ -2,10 +2,14 @@ package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
 
 public class Tile {
   private TileAction action;
+  private int tileNumber;
 
   public void landingAction(GamePiece gamePiece){
     if(action != null){
       action.Action(gamePiece);
     }
+  }
+  public int getTileNumber() {
+    return tileNumber;
   }
 }
