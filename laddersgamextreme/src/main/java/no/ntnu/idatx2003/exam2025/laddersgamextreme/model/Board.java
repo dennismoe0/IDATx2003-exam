af39d480553ea7b2c653ce36b0e5638d7cf20ad2
Ink-Song <sunruh@proton.me>
2025-03-21 12:28:08 +0100

    Added GamePiece Class

--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Board.java
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Board.java
@@ -1,5 +1,15 @@
 package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
 
+import java.util.ArrayList;
+
 public class Board {
+  ArrayList<Tile> tiles = new ArrayList<Tile>();
+
+  public ArrayList<Tile> getTiles() {
+    return tiles;
+  }
+  public Tile getTile(int position) {
+    return tiles.get(position);
+  }
 
 }
--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Tile.java
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Tile.java
@@ -1,4 +1,9 @@
 package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
 
 public class Tile {
+  private TileAction action;
+
+  public void landingAction(GamePiece gamePiece){
+
+  }
 }
