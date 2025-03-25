98af44730b1834e0eb953fc0122ad4595890ee06
Ink-Song <sunruh@proton.me>
2025-03-25 16:38:37 +0100

    Feature: Added Dice class

---
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Dice.java
@@ -0,0 +1,31 @@
+package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
+
+import java.util.Random;
+
+public class Dice {
+  private int diceSize;
+  private Random rand = new Random();
+  private int lastRoll;
+  public Dice(int diceSize) {
+    this.diceSize = diceSize;
+  }
+  public Dice(){
+    this.diceSize = 6;
+  }
+
+  public int roll(){
+    this.lastRoll = rand.nextInt(diceSize);
+    return this.lastRoll;
+  }
+  public void setDiceSize(int diceSize) {
+    this.diceSize = diceSize;
+  }
+  public int getDiceSize() {
+    return this.diceSize;
+  }
+  public int getLastRoll() {
+    return this.lastRoll;
+  }
+
+
+}
