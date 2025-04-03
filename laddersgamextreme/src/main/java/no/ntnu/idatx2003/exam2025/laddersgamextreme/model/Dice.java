0af0a710350c96f5fb6b212cdad5004217c6806e
dennismoe0 <dennismoe0@gmail.com>
2025-04-03 12:22:45 +0200

    We're going to recreate all source files after planning a new structure based on more robust Patterns, Interfaces and structures.

--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Dice.java
+++ laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/Dice.java
@@ -6,26 +6,30 @@ public class Dice {
   private int diceSize;
   private Random rand = new Random();
   private int lastRoll;
+
   public Dice(int diceSize) {
     this.diceSize = diceSize;
   }
-  public Dice(){
+
+  public Dice() {
     this.diceSize = 6;
   }
 
-  public int roll(){
+  public int roll() {
     this.lastRoll = rand.nextInt(diceSize) + 1;
     return this.lastRoll;
   }
+
   public void setDiceSize(int diceSize) {
     this.diceSize = diceSize;
   }
+
   public int getDiceSize() {
     return this.diceSize;
   }
+
   public int getLastRoll() {
     return this.lastRoll;
   }
-
 
 }
--- laddersgamextreme/src/main/java/no/ntnu/idatx2003/exam2025/laddersgamextreme/model/TileAction.java
+++
@@ -1,5 +0,0 @@
-package no.ntnu.idatx2003.exam2025.laddersgamextreme.model;
-
-public interface TileAction {
-  void Action(GamePiece gamePiece);
-}
