56b57feaf13bdbb26c04c6a6ce76d04bfc538456
Ink-Song <sunruh@proton.me>
2025-04-18 12:48:49 +0200

    Added test logic to GamePiece test

--- boardgames/src/test/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePieceTest.java
+++ boardgames/src/test/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePieceTest.java
@@ -1,20 +1,52 @@
 package no.ntnu.idatx2003.exam2025.boardgames.model.player;
 
+import no.ntnu.idatx2003.exam2025.boardgames.model.board.Board;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.EmptyTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamePieceTest {
 
+  private static GamePiece gamePiece;
+  private static Board board;
+
+
+  @BeforeAll
+  static void setUp(){
+    gamePiece = new GamePiece();
+    board = new Board();
+    for (int i = 0; i < 12; i++) {
+      board.setTile(i,new Tile(i, new EmptyTileStrategy()));
+    }
+  }
+  @BeforeEach
+  void setUpGamePiece(){
+    gamePiece.setCurrentTile(board.getTile(0));
+  }
+
   @Test
   void getCurrentTile() {
+    assertEquals(gamePiece.getCurrentTile(),board.getTile(0));
+    gamePiece.setCurrentTile(board.getTile(1));
+    assertEquals(gamePiece.getCurrentTile(),board.getTile(1));
   }
 
   @Test
   void setCurrentTile() {
+    assertEquals(gamePiece.getCurrentTile(),board.getTile(0));
+    gamePiece.setCurrentTile(board.getTile(1));
+    assertEquals(gamePiece.getCurrentTile(),board.getTile(1));
   }
 
   @Test
   void move() {
+    System.out.println(gamePiece.getCurrentTile().getId());
+    gamePiece.move(6);
+    System.out.println(gamePiece.getCurrentTile().getId());
+    assertEquals(gamePiece.getCurrentTile(),board.getTile(5));
   }
 }
