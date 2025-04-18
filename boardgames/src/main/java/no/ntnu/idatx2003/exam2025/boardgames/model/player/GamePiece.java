73e3c29e203d0f40ba30e27a05a2704f40316d65
Ink-Song <sunruh@proton.me>
2025-04-18 12:37:20 +0200

    Added check to ensure game piece stays in bounds.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
@@ -2,12 +2,22 @@ package no.ntnu.idatx2003.exam2025.boardgames.model.player;
 
 import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
 
+/**
+ * Basic piece that can move around the game board.
+ */
 public class GamePiece {
   private Tile currentTile;
 
+  /**
+   * A function for moving the game piece.
+   * @param steps An integer getting the number of spaces the piece will move.
+   */
   public void move(int steps){
     for (int i = 0; i < steps; i++) {
-      currentTile = currentTile.getNextTile();
+      if(currentTile.getNextTile() != null){
+        currentTile = currentTile.getNextTile();
+      }
+      else{break;}
     }
     currentTile.getTileStrategy().applyEffect(this);
   }
