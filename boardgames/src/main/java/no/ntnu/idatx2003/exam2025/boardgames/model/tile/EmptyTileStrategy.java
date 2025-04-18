05cbc90f02cf55b85dee42a518b248ca35bc04ea
Ink-Song <sunruh@proton.me>
2025-04-18 12:53:42 +0200

    Small modification to emtpyTileStrategy for readability purposes.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/EmptyTileStrategy.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/EmptyTileStrategy.java
@@ -5,7 +5,5 @@ import no.ntnu.idatx2003.exam2025.boardgames.model.player.GamePiece;
 public class EmptyTileStrategy implements TileStrategy {
 
   @Override
-  public void applyEffect(GamePiece gamePiece) {
-    return;
-  }
+  public void applyEffect(GamePiece gamePiece) {}
 }
