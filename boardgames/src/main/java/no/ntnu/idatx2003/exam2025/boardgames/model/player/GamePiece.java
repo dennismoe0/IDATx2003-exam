0b560ffce3a619b9df502538c83779b8bde55aa2
Ink-Song <sunruh@proton.me>
2025-04-18 12:31:02 +0200

    Added move function to game piece

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
@@ -5,4 +5,9 @@ import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
 public class GamePiece {
   private Tile currentTile;
 
+  public void move(int steps){
+    for (int i = 0; i < steps; i++) {
+      currentTile = currentTile.getNextTile();
+    }
+  }
 }
