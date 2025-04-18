cfc79d206c4cb1ef3cc23867b263322fa4e589a2
Ink-Song <sunruh@proton.me>
2025-04-18 12:41:30 +0200

    Added logging to game piece and created test class

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
@@ -1,18 +1,34 @@
 package no.ntnu.idatx2003.exam2025.boardgames.model.player;
 
 import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
+import no.ntnu.idatx2003.exam2025.boardgames.util.Log;
+import org.slf4j.Logger;
 
 /**
  * Basic piece that can move around the game board.
  */
 public class GamePiece {
+  private static final Logger log = Log.get(GamePiece.class);
+  private Tile previousTile;
   private Tile currentTile;
+
+  public GamePiece() {
+    currentTile = null;
+    log.debug("GamePiece created");
+  }
+  public Tile getCurrentTile() {
+    return currentTile;
+  }
+  public void setCurrentTile(Tile tile) {
+    currentTile = tile;
+  }
 
   /**
    * A function for moving the game piece.
    * @param steps An integer getting the number of spaces the piece will move.
    */
   public void move(int steps){
+    this.previousTile = currentTile;
     for (int i = 0; i < steps; i++) {
       if(currentTile.getNextTile() != null){
         currentTile = currentTile.getNextTile();
@@ -20,5 +36,6 @@ public class GamePiece {
       else{break;}
     }
     currentTile.getTileStrategy().applyEffect(this);
+    log.debug("Moved {} steps, from tile({}) to tile({})", steps, previousTile.getId(), currentTile.getId());
   }
 }
---
+++ boardgames/src/test/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePieceTest.java
@@ -0,0 +1,20 @@
+package no.ntnu.idatx2003.exam2025.boardgames.model.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamePieceTest {
+
+  @Test
+  void getCurrentTile() {
+  }
+
+  @Test
+  void setCurrentTile() {
+  }
+
+  @Test
+  void move() {
+  }
+}
