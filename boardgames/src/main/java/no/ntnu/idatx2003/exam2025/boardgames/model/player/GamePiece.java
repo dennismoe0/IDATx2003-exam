45a1e3b6d67948c1bc655428df66413d91521cbc
Ink-Song <sunruh@proton.me>
2025-04-18 12:25:13 +0200

    Added GamePiece class.

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/player/GamePiece.java
@@ -0,0 +1,8 @@
+package no.ntnu.idatx2003.exam2025.boardgames.model.player;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
+
+public class GamePiece {
+  private Tile currentTile;
+
+}
