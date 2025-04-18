fa093f049e6275c14696ad04f93c37cc9551c56b
dennismoe0 <dennismoe0@gmail.com>
2025-04-18 12:31:02 +0200

    Made a dice class that uses a list of Die.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/Dice.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/Dice.java
@@ -3,6 +3,58 @@ package no.ntnu.idatx2003.exam2025.boardgames.model;
 import no.ntnu.idatx2003.exam2025.boardgames.util.Log;
 import org.slf4j.Logger;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a list of Die from the Die class.
+ * Gives output based on rolling of multiple dice.
+ */
 public class Dice {
 
+  private static final Logger log = Log.get(Dice.class);
+  private static final Random RNG = new Random();
+
+  private final List<Die> dice;
+
+  /**
+   * Constructs a Dice object with a list of Die.
+   *
+   * @param dice a list of Die objects (non-null/empty)
+   * @throws IllegalArgumentException if the list is null/empty
+   */
+  public Dice(List<Die> dice) {
+    if (dice == null || dice.isEmpty()) {
+      log.error("Tried to create Dice with null or empty list.");
+      throw new IllegalArgumentException("Need at least 1 die in the list.");
+    }
+    this.dice = dice;
+    log.debug("Successfully created Dice with {} dice", dice.size());
+  }
+
+  /**
+   * Calls the roll method for each individual die.
+   * Did this instead of pretending to roll to be more Object Oriented.
+   *
+   * @return A list of each individual dice roll.
+   */
+  public List<Integer> rollAllDice() {
+    List<Integer> results = dice.stream()
+        .map(Die::roll).collect(Collectors.toList());
+
+    log.info("Rolled all dice: {}", results);
+    return results;
+  }
+
+  /**
+   * Uses the rollAllDice method to get a sum-value of the rolled dice.
+   *
+   * @return A whole number representing the combined value of rolls.
+   */
+  public int rollAllDiceSum() {
+    int sum = rollAllDice().stream().mapToInt(Integer::intValue).sum();
+    log.info("Sum of all dice rolls: {}, sum");
+    return sum;
+  }
 }
