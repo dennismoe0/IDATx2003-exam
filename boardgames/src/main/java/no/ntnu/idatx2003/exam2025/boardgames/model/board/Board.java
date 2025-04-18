85973a27b77856b75d60acf93ae3a5239c739354
Ink-Song <sunruh@proton.me>
2025-04-18 12:20:02 +0200

    Add test class for Board

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/board/Board.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/board/Board.java
@@ -4,10 +4,20 @@ import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
 
 import java.util.HashMap;
 
+import no.ntnu.idatx2003.exam2025.boardgames.util.Log;
+import org.slf4j.Logger;
+
+/**
+ * Basic Board class to be used for making Tile Based board games.
+ * Holds a hashmap of tile objects by ID.
+ */
 public class Board{
   private HashMap<Integer, Tile> tiles;
+  private static final Logger log = Log.get(Board.class);
+
   public Board(){
     tiles = new HashMap<>();
+    log.debug("Board created");
   }
   public Tile getTile(int id){
     return tiles.get(id);
@@ -15,6 +25,7 @@ public class Board{
   public void setTile(int id, Tile tile){
     tiles.put(id, tile);
   }
+
   public void removeTile(int id){
     tiles.remove(id);
   }
