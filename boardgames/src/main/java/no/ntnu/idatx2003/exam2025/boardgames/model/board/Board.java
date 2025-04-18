bd946cdbf976a04445f0f797617668a7e5955237
Ink-Song <sunruh@proton.me>
2025-04-18 12:05:46 +0200

    Created Board Class

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/board/Board.java
@@ -0,0 +1,21 @@
+package no.ntnu.idatx2003.exam2025.boardgames.model.board;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
+
+import java.util.HashMap;
+
+public class Board{
+  private HashMap<Integer, Tile> tiles;
+  public Board(){
+    tiles = new HashMap<>();
+  }
+  public Tile getTile(int id){
+    return tiles.get(id);
+  }
+  public void setTile(int id, Tile tile){
+    tiles.put(id, tile);
+  }
+  public void removeTile(int id){
+    tiles.remove(id);
+  }
+}
