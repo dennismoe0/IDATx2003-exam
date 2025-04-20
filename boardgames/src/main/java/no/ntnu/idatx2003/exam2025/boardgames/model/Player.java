4d697378d53ba1e7739f967daf555baee8027f00
Dennis Moe <dennismoe0@gmail.com>
2025-04-20 22:05:26 +0200

    Changed constructor to work based on new logic.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/Player.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/Player.java
@@ -3,7 +3,6 @@ package no.ntnu.idatx2003.exam2025.boardgames.model;
 import java.util.ArrayList;
 import java.util.List;
 import no.ntnu.idatx2003.exam2025.boardgames.model.stats.PlayerStats;
-import no.ntnu.idatx2003.exam2025.boardgames.model.stats.DefaultPlayerStats;
 import no.ntnu.idatx2003.exam2025.boardgames.util.Log;
 import org.slf4j.Logger;
 
@@ -25,8 +24,8 @@ public class Player {
    */
   public Player(int playerId, String playerName, int playerAge) {
     this.playerId = playerId;
+    this.playerStats = null; // Injected later
     this.playerName = playerName;
-    this.playerStats = new DefaultPlayerStats(playerName);
     this.playerAge = playerAge;
   }
 
@@ -37,9 +36,11 @@ public class Player {
    *                    player to load.
    * @param playerStats Unique stats connected to player-profile.
    */
-  public Player(int playerId, PlayerStats playerStats) {
+  public Player(int playerId, PlayerStats playerStats, String playerName, int playerAge) {
     this.playerId = playerId;
     this.playerStats = playerStats;
+    this.playerName = playerName;
+    this.playerAge = playerAge;
   }
 
   public int getPlayerId() {
@@ -66,58 +67,60 @@ public class Player {
     this.playerAge = playerAge;
   }
 
-  // TO BE REMOVED / MOVED TO GameSession
+  // // TO BE REMOVED / MOVED TO GameSession
 
-  /**
-   * Adds a game piece to the player's collection of game pieces.
-   *
-   * @param gamePiece The game piece to be added to the player.
-   */
-  public void addGamePieceToPlayer(GamePiece gamePiece) {
-    gamePieces.add(gamePiece);
-    log.debug("Added a GamePiece to player {}", playerName);
-  }
+  // /**
+  // * Adds a game piece to the player's collection of game pieces.
+  // *
+  // * @param gamePiece The game piece to be added to the player.
+  // */
+  // public void addGamePieceToPlayer(GamePiece gamePiece) {
+  // gamePieces.add(gamePiece);
+  // log.debug("Added a GamePiece to player {}", playerName);
+  // }
 
-  /**
-   * Removes a game piece from the player's collection of game pieces.
-   *
-   * @param gamePiece The game piece to be removed from the player.
-   */
-  public void removeGamePieceFromPlayer(GamePiece gamePiece) {
-    gamePieces.remove(gamePiece);
-    log.debug("Removed a GamePiece from player {}.", playerName);
-  }
+  // /**
+  // * Removes a game piece from the player's collection of game pieces.
+  // *
+  // * @param gamePiece The game piece to be removed from the player.
+  // */
+  // public void removeGamePieceFromPlayer(GamePiece gamePiece) {
+  // gamePieces.remove(gamePiece);
+  // log.debug("Removed a GamePiece from player {}.", playerName);
+  // }
 
-  public List<GamePiece> getGamePieces() {
-    if (gamePieces.isEmpty()) {
-      log.debug("There are no Game Pieces assigned to {}", playerName);
-      return gamePieces;
-    }
-    return gamePieces;
-  }
+  // public List<GamePiece> getGamePieces() {
+  // if (gamePieces.isEmpty()) {
+  // log.debug("There are no Game Pieces assigned to {}", playerName);
+  // return gamePieces;
+  // }
+  // return gamePieces;
+  // }
 
-  /**
-   * Checks if the player has any game pieces assigned.
-   *
-   * @return true if the player has one or more game pieces, false otherwise.
-   */
-  public boolean playerHasGamePieces() {
-    return !gamePieces.isEmpty();
-  }
+  // /**
+  // * Checks if the player has any game pieces assigned.
+  // *
+  // * @return true if the player has one or more game pieces, false otherwise.
+  // */
+  // public boolean playerHasGamePieces() {
+  // return !gamePieces.isEmpty();
+  // }
 
-  public GamePiece getGamePiece(int index) {
-    if (!playerHasGamePieces()) {
-      throw new IllegalArgumentException("Player (" + playerName + ") has no Game Pieces assigned.");
-    }
-    if (index < 0 || index >= gamePieces.size()) {
-      throw new IndexOutOfBoundsException("No game piece at index: " + index + ".");
-    }
-    return gamePieces.get(index);
-  }
+  // public GamePiece getGamePiece(int index) {
+  // if (!playerHasGamePieces()) {
+  // throw new IllegalArgumentException("Player (" + playerName + ") has no Game
+  // Pieces assigned.");
+  // }
+  // if (index < 0 || index >= gamePieces.size()) {
+  // throw new IndexOutOfBoundsException("No game piece at index: " + index +
+  // ".");
+  // }
+  // return gamePieces.get(index);
+  // }
 
-  public void clearAllGamePieces() {
-    gamePieces.clear();
-    log.debug("Removed all gamePieces from {}", playerName);
-  }
+  // public void clearAllGamePieces() {
+  // gamePieces.clear();
+  // log.debug("Removed all gamePieces from {}", playerName);
+  // }
 
 }
