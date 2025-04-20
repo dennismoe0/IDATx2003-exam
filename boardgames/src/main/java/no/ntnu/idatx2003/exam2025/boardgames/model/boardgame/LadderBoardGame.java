c19d2f6f356f65e9a5b91b386aa06d07d05c5924
Ink-Song <sunruh@proton.me>
2025-04-20 20:12:32 +0200

    Added Hashmap to track player pieces in board game

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/boardgame/LadderBoardGame.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/boardgame/LadderBoardGame.java
@@ -4,16 +4,29 @@ import no.ntnu.idatx2003.exam2025.boardgames.model.Dice;
 import no.ntnu.idatx2003.exam2025.boardgames.model.GamePiece;
 import no.ntnu.idatx2003.exam2025.boardgames.model.Player;
 
-public class LadderBoardGame extends BoardGame {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LadderBoardGame extends BoardGame {
+  private int NUMBER_OF_PIECES = 1;
+  private int NUMBER_OF_DICE = 1;
   private Dice dice;
-  public LadderBoardGame(Dice dice) {
-    this.dice = dice;
+  private GamePiece currentGamePiece;
+
+
+  @Override
+  public void setUp(List<Player> players){
+    List<GamePiece> pieces = new ArrayList<GamePiece>();
+    for (Player player : players) {
+      pieces.clear();
+      pieces.add(new GamePiece());
+      super.addPlayerPieces(player, pieces);
+    }
   }
 
   @Override
-  public void takeTurn(GamePiece gamePiece) {
-    gamePiece.move(dice.rollAllDiceSum());
+  public void takeTurn(Player player) {
+
   }
 
 
