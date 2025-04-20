0d3f078e65dbfc913da819c16732c6d0a53237ab
Ink-Song <sunruh@proton.me>
2025-04-20 20:55:22 +0200

    Added default ladder board to Board Factory

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/board/BoardFactory.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/board/BoardFactory.java
@@ -1,5 +1,28 @@
 package no.ntnu.idatx2003.exam2025.boardgames.model.board;
 
-public class BoardFactory {
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.EmptyTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
 
+public class BoardFactory {
+  private Board board;
+
+  public Board createDefaultLadderBoard(){
+    board = new Board();
+    Tile tile;
+    //initialize the tiles
+    //When reading from an XML we'll break the initialize and assemble into separate steps.
+    for (int i = 0; i < 90; i++) {
+      //switch statement to handle assigning tile functions.
+      switch (i){
+        case 0:
+
+        default:
+          tile = new Tile(i,new EmptyTileStrategy());
+      }
+
+    }
+
+
+    return board;
+  }
 }
