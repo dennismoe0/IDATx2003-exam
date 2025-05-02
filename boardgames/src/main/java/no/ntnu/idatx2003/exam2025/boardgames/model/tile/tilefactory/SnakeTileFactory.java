99f0a4e6410727ef8de0d92066cc3da740530fa8
Ink-Song <sunruh@proton.me>
2025-05-02 11:01:21 +0200

    Added a snakeTileFactory

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/tilefactory/SnakeTileFactory.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/tilefactory/SnakeTileFactory.java
@@ -3,19 +3,19 @@ package no.ntnu.idatx2003.exam2025.boardgames.model.tile.tilefactory;
 import com.google.gson.JsonObject;
 import no.ntnu.idatx2003.exam2025.boardgames.model.board.Board;
 import no.ntnu.idatx2003.exam2025.boardgames.model.tile.LadderTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.SnakeTileStrategy;
 import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
 
 /**
  * Factory for building a Ladder Tile.
  */
-public class LadderTileFactory implements TileFactory {
-
+public class SnakeTileFactory implements TileFactory {
   @Override
   public Tile tileFromJson(JsonObject json, Board board) {
     int id = json.get("id").getAsInt();
     int exitId = json.get("exit-tile").getAsInt();
 
-    LadderTileStrategy strategy = new LadderTileStrategy(board.getTile(id), board.getTile(exitId));
+    SnakeTileStrategy strategy = new SnakeTileStrategy(board.getTile(id), board.getTile(exitId));
 
     return new Tile(id, strategy);
   }
