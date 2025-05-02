c3edcfc4b257a250b1b0fe37b62a49efbe74cf94
Ink-Song <sunruh@proton.me>
2025-05-02 10:41:35 +0200

    Added GSOn dependency

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/tilefactory/TileFactory.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/tile/tilefactory/TileFactory.java
@@ -1,4 +1,16 @@
 package no.ntnu.idatx2003.exam2025.boardgames.model.tile.tilefactory;
 
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.Tile;
+
+/**
+ * Default tile factory interface for use with creating new Tile Factories.
+ */
 public interface TileFactory {
+  /**
+   * Default method for Tile Factories. Returns a Tile parsed from a Json object.
+   *
+   * @param json the JSOn code to be used.
+   * @return Returns a Tile object.
+   */
+  Tile tileFromJson(String json);
 }
