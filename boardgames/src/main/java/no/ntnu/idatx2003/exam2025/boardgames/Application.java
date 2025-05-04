43bf52ea88aadf3b0c4b6db862443da3cc44b211
Ink-Song <sunruh@proton.me>
2025-05-04 11:28:11 +0200

    Created a main application class and a menu view class.

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/Application.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatx2003.exam2025.boardgames;
+
+/**
+ * The main Application File for starting and stopping the program.
+ */
+public class Application {
+  /**
+   * The main method for running the program.
+   *
+   * @param args default args.
+   */
+  public static void main(String[] args) {
+
+  }
+}
---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/MenuView.java
@@ -0,0 +1,8 @@
+package no.ntnu.idatx2003.exam2025.boardgames.view;
+
+/**
+ * Custom container class for displaying menu options visually.
+ */
+public class MenuView {
+
+}
