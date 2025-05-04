0d45672b079a13b5b4fd8a4fdff79d5b7380cca5
Ink-Song <sunruh@proton.me>
2025-05-04 13:44:12 +0200

    Made a scene manager but need to rethink implementation

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/service/SceneManager.java
@@ -0,0 +1,20 @@
+package no.ntnu.idatx2003.exam2025.boardgames.service;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Manager used to change scenes/navigate the program.
+ */
+public class SceneManager {
+  private Stage stage;
+
+  /**
+   * A method that can be called to change scenes in the program.
+   *
+   * @param root the root node for the scene to be swapped to.
+   */
+  public void changeRoot(Node root) {
+  }
+}
