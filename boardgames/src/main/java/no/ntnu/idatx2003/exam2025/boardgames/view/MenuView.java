b3265fd68d18bb25f3e3bff54947474d74705ed9
Ink-Song <sunruh@proton.me>
2025-05-05 15:35:53 +0200

    Modified menuView for basic construction.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/MenuView.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/MenuView.java
@@ -1,24 +1,44 @@
 package no.ntnu.idatx2003.exam2025.boardgames.view;
 
+import java.util.List;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-import java.util.List;
 
 /**
  * Custom container class for displaying menu options visually.
  */
 public class MenuView {
   private Text title;
-  private BorderPane view;
+  private final BorderPane view;
   private VBox menu;
-  private List<MenuOption> options;
+  private final List<MenuOption> options;
 
-
+  /**
+   * Default constructor for a MenuView
+   *
+   * @param title   the title of the Menu to be displayed.
+   * @param options A list of Menu options (buttons).
+   */
   public MenuView(String title, List<MenuOption> options) {
     view = new BorderPane();
     this.title = new Text(title);
+    this.options = options;
+    createView();
+    createView();
+    view.setTop(this.title);
+    view.setCenter(menu);
+  }
+
+  /**
+   * For retrieving the parent object to change roots.
+   *
+   * @return returns the parent object of the Menu.
+   */
+  public Parent asParent() {
+    return view;
   }
 
   private void createView() {
