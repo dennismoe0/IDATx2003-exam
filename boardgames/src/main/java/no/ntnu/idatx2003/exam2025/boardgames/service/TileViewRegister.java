84a509a92d5942dc41b1ac35a0c28da5c2782714
Ink-Song <sunruh@proton.me>
2025-05-06 15:41:18 +0200

    Filled TileViewRegister with examples.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/service/TileViewRegister.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/service/TileViewRegister.java
@@ -1,5 +1,26 @@
 package no.ntnu.idatx2003.exam2025.boardgames.service;
 
-public class TileViewRegister {
+import java.util.HashMap;
+import java.util.Map;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.EmptyTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.LadderTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.SnakeTileStrategy;
+import no.ntnu.idatx2003.exam2025.boardgames.model.tile.TileStrategy;
 
+public class TileViewRegister {
+  private Map<Class<? extends TileStrategy>, String> registry = new HashMap<>();
+
+  public TileViewRegister() {
+    register(EmptyTileStrategy.class, "ts-empty");
+    register(LadderTileStrategy.class, "ts-ladder");
+    register(SnakeTileStrategy.class, "ts-snake");
+  }
+
+  public void register(Class<? extends TileStrategy> strategy, String name) {
+    registry.put(strategy, name);
+  }
+
+  public String get(Class<? extends TileStrategy> strategy) {
+    return registry.get(strategy);
+  }
 }
