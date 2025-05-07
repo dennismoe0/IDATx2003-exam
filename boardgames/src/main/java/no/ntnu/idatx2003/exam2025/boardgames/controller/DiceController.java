672d801c3dc681d2286e6f42a347195fdea13147
Ink-Song <sunruh@proton.me>
2025-05-07 12:43:04 +0200

    Added javadoc to dice controller.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/controller/DiceController.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/controller/DiceController.java
@@ -2,11 +2,24 @@ package no.ntnu.idatx2003.exam2025.boardgames.controller;
 
 import no.ntnu.idatx2003.exam2025.boardgames.model.Dice;
 
+/**
+ * Controller for communicating between dice and View.
+ */
 public class DiceController {
   private Dice dice;
+
+  /**
+   * The default Dice Controller constructor.
+   *
+   * @param dice the dice from the selected Board Game.
+   */
   public DiceController(Dice dice) {
     this.dice = dice;
   }
+
+  /**
+   * A method for rolling the dice. Returns void.
+   */
   public void rollDice() {
     dice.rollAllDiceSum();
   }
