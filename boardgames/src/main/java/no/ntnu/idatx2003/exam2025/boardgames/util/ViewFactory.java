bf6b0e9da358f27c09306854dede06a41668ec50
Ink-Song <sunruh@proton.me>
2025-05-13 17:59:31 +0200

    Created StartGameCommand and ViewFactory classes

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/ViewFactory.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatx2003.exam2025.boardgames.util;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.board.Board;
+import no.ntnu.idatx2003.exam2025.boardgames.model.boardgame.BoardGame;
+import no.ntnu.idatx2003.exam2025.boardgames.model.boardgame.LadderBoardGame;
+import no.ntnu.idatx2003.exam2025.boardgames.view.BoardGameView;
+
+public class ViewFactory {
+
+  public BoardGameView buildLadderBoardGameView(LadderBoardGame LadderBoardGame) {
+
+  }
+}
---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/command/StartGameCommand.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatx2003.exam2025.boardgames.util.command;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.GameSession;
+
+public class StartGameCommand implements Command {
+  private GameSession gameSession;
+
+  /**
+   * Execute function used to perform whatever command needs to be performed.
+   */
+  @Override
+  public void execute() {
+
+  }
+}
