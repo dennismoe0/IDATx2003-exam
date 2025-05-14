60e205ec39981dfd457decff201b074301b64da7
Ink-Song <sunruh@proton.me>
2025-05-14 14:12:24 +0200

    Created titledFieldView

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/TitledFieldView.java
@@ -0,0 +1,33 @@
+package no.ntnu.idatx2003.exam2025.boardgames.view;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class TitledFieldView {
+  private VBox root;
+  private TextField field;
+  private Label label;
+  private String title;
+  private String placeHolder;
+  private float width;
+  private float height;
+
+  public TitledFieldView(String title, String placeHolder, float width, float height) {
+    this.title = title;
+    this.placeHolder = placeHolder;
+    this.width = width;
+    this.height = height;
+    root = new VBox();
+    configureField();
+  }
+
+  private void configureField() {
+    label = new Label(title);
+    field = new TextField();
+    field.setPrefWidth(width);
+    field.setPrefHeight(height);
+    field.setPromptText(placeHolder);
+    root.getChildren().addAll(label, field);
+  }
+}
