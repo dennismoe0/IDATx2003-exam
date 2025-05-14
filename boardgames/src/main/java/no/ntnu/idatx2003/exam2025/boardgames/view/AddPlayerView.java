ea304276fcfae22c0c3b18930a79da72ab7dcd22
Ink-Song <sunruh@proton.me>
2025-05-14 14:06:28 +0200

    Created AddPlayerView

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/AddPlayerView.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatx2003.exam2025.boardgames.view;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
+public class AddPlayerView {
+  private StackPane root;
+  private Rectangle backdrop;
+  private Label title;
+  private static final float width = 780;
+  private static final float height = 540;
+  private VBox layout;
+}
