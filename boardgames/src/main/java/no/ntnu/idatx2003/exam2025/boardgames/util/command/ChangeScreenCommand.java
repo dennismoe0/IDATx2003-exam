186f8b0a0dc0f7e5bf815a19d525b3dbcb436e87
Ink-Song <sunruh@proton.me>
2025-05-14 12:30:01 +0200

    Added javadoc to ChangeScreenCommand

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/command/ChangeScreenCommand.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/command/ChangeScreenCommand.java
@@ -7,21 +7,28 @@ import no.ntnu.idatx2003.exam2025.boardgames.service.SceneRegister;
  * A command used to change to a new screen from within the program.
  */
 public class ChangeScreenCommand implements Command {
-  private SceneRegister register;
-  private SceneManager manager;
-  private String sceneName;
+  private final SceneRegister register;
+  private final SceneManager manager;
+  private final String sceneKey;
   //Need a scene Manager.
 
-  public ChangeScreenCommand(SceneRegister register, SceneManager manager, String input) {
+  /**
+   * Default constructor for a ChangeScreenCommand.
+   *
+   * @param register the Scene Register that calls a build view.
+   * @param manager  the scene manager responsible for swapping scenes.
+   * @param key      the String that will be checked in the register.
+   */
+  public ChangeScreenCommand(SceneRegister register, SceneManager manager, String key) {
     this.register = register;
     this.manager = manager;
-    this.sceneName = input;
+    this.sceneKey = key;
   }
 
   /**
    * A method for executing the given command and changing screens.
    */
   public void execute() {
-    manager.changeRoot(register.get(sceneName));
+    manager.changeRoot(register.get(sceneKey));
   }
 }
