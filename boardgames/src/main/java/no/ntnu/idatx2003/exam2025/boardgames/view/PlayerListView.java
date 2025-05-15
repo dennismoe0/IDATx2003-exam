bba4ee85b19a9c46126c3dc6b2a6b42aa8b5c1d3
Ink-Song <sunruh@proton.me>
2025-05-15 14:16:56 +0200

    Built constructor method for PlayerListView

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/PlayerListView.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/PlayerListView.java
@@ -1,5 +1,18 @@
 package no.ntnu.idatx2003.exam2025.boardgames.view;
 
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerListView {
-  //query the list of existing players.
+  private final List<PlayerItemView> playerItemViews;
+  private final VBox layout;
+
+
+  public PlayerListView() {
+    playerItemViews = new ArrayList<>();
+    layout = new VBox(2);
+  }
 }
