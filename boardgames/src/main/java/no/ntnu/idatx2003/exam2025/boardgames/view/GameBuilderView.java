5e1df70e6e8e9af4e3c1989e36a3e2e0fc1a0d3c
Ink-Song <sunruh@proton.me>
2025-05-15 14:01:36 +0200

    Added GameBuilderView

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/GameBuilderView.java
@@ -0,0 +1,14 @@
+package no.ntnu.idatx2003.exam2025.boardgames.view;
+
+/**
+ * View for displaying options for populating and starting a game session.
+ */
+public class GameBuilderView {
+  //player list
+  //game chooser (drop down menu?)
+  //player picker/color picker/icon picker?
+  //Game rules (probably board based but whatever)
+  //start button
+  //board picker
+
+}
