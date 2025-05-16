8967b1ffd65887f2e419f12673af88279fb7a51d
Dennis Moe <dennismoe0@gmail.com>
2025-05-16 17:41:13 +0200

    New idea for visual pieces

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/GamePieceView.java
@@ -0,0 +1,16 @@
+package no.ntnu.idatx2003.exam2025.boardgames.view;
+
+public class GamePieceView {
+  // After BoardView creation -> Get a list from boardgame class (i.e.
+  // ladderboardgame)
+  // get list a gamepieces from the boardgame (ladderboardgame, input based)
+  // BoardView shows the board, so it needs to be on top of that.
+  // -> Update gamepiece with observable ID for it's current tile position that is
+  // used for movement.
+  // GamePieces should be able to be on the same tile (rezising), and should be
+  // connected visually to the tile their on
+  // I.e. if a piece from player 1 is on tile 4, it should be shown there on top
+  // of BoardView's view of the board.
+  // Since BoardView is a part of BoardGameView this should be a part of that.
+  // Summary: Visual pieces based on backend location.
+}
