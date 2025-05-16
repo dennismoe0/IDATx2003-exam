b7ec68abb367cc6cf4a7c5a0e38c4028335b9fde
Ink-Song <sunruh@proton.me>
2025-05-16 16:24:29 +0200

    Added Double Movement Strategy

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/gamepiece/DoubleMovementStrategy.java
@@ -0,0 +1,29 @@
+package no.ntnu.idatx2003.exam2025.boardgames.model.gamepiece;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.GamePiece;
+
+/**
+ * Movement strategy for moving the player twice as far.
+ */
+public class DoubleMovementStrategy implements MovementStrategy {
+  /**
+   * Movement function for computing how the piece moves.
+   *
+   * @param roll the number the player rolled on the dice.
+   * @return returns an integer as the number of spaces the player should move.
+   */
+  @Override
+  public int computeMovement(int roll) {
+    return roll * 2;
+  }
+
+  /**
+   * Utility method for movement strategies (in case they're turn based!)
+   *
+   * @param gamePiece the game piece to be affected, if necessary.
+   */
+  @Override
+  public void onTurnEnd(GamePiece gamePiece) {
+    gamePiece.setMovementStrategy(new BasicMovementStrategy());
+  }
+}
