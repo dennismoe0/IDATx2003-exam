c4626ab67b666206a845da9ac05b0d774529a933
Ink-Song <sunruh@proton.me>
2025-05-16 15:39:19 +0200

    Added a basic movement strategy.

---
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/model/gamepiece/BasicMovementStrategy.java
@@ -0,0 +1,31 @@
+package no.ntnu.idatx2003.exam2025.boardgames.model.gamepiece;
+
+import no.ntnu.idatx2003.exam2025.boardgames.model.GamePiece;
+
+/**
+ * The default movement strategy for the game.
+ */
+public class BasicMovementStrategy implements MovementStrategy {
+
+  /**
+   * Movement function for computing how the piece moves.
+   *
+   * @param roll      the number the player rolled on the dice.
+   * @param gamePiece the gamepiece to be moved/have it's movement applied.
+   * @return returns an integer as the number of spaces the player should move.
+   */
+  @Override
+  public int computeMovement(int roll, GamePiece gamePiece) {
+    return roll;
+  }
+
+  /**
+   * Utility method for movement strategies (in case they're turn based!).
+   *
+   * @param gamePiece the game piece to be affected, if necessary.
+   */
+  @Override
+  public void onTurnEnd(GamePiece gamePiece) {
+
+  }
+}
