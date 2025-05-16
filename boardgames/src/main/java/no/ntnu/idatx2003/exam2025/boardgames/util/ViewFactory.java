1d5096d074d98b39a55ee59a69eecdf73d2550ae
Dennis Moe <dennismoe0@gmail.com>
2025-05-16 17:26:59 +0200

    Javadoc and formatting

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/ViewFactory.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/util/ViewFactory.java
@@ -1,14 +1,13 @@
 package no.ntnu.idatx2003.exam2025.boardgames.util;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javafx.scene.Parent;
 import no.ntnu.idatx2003.exam2025.boardgames.controller.AddPlayerViewController;
 import no.ntnu.idatx2003.exam2025.boardgames.controller.GameBuilderController;
 import no.ntnu.idatx2003.exam2025.boardgames.controller.PlayerListViewController;
+import no.ntnu.idatx2003.exam2025.boardgames.dao.player.PlayerDaoImpl;
 import no.ntnu.idatx2003.exam2025.boardgames.model.GameSession;
 import no.ntnu.idatx2003.exam2025.boardgames.model.boardgame.BoardGame;
 import no.ntnu.idatx2003.exam2025.boardgames.model.boardgame.LadderBoardGame;
@@ -18,17 +17,17 @@ import no.ntnu.idatx2003.exam2025.boardgames.service.StatsManager;
 import no.ntnu.idatx2003.exam2025.boardgames.util.command.ChangeScreenCommand;
 import no.ntnu.idatx2003.exam2025.boardgames.util.command.PrintLineCommand;
 import no.ntnu.idatx2003.exam2025.boardgames.view.*;
-
 import org.slf4j.Logger;
 
-import no.ntnu.idatx2003.exam2025.boardgames.dao.player.PlayerDaoImpl;
-
+/**
+ * Factory class for building various views used in the board games application.
+ */
 public class ViewFactory {
 
   private static final Logger logger = Log.get(ViewFactory.class);
 
   /**
-   * Default method for making a LadderBoardGameView
+   * Default method for making a LadderBoardGameView.
    *
    * @param boardGame the game to be built.
    * @return returns a BoardGameView object.
@@ -39,12 +38,21 @@ public class ViewFactory {
     return boardGameView.asParent();
   }
 
+  /**
+   * Builds the main menu view.
+   *
+   * @param sceneRegister the scene register
+   * @param sceneManager  the scene manager
+   * @return the root node of the main menu view
+   */
   public Parent buildMainMenuView(SceneRegister sceneRegister, SceneManager sceneManager) {
     logger.info("Building main menu");
     List<MenuOption> menuOptions = new ArrayList<>();
     menuOptions.add(new MenuOption("Start", new PrintLineCommand("Start"), true));
     menuOptions.add(new MenuOption(
-        "Swap to Board View", new ChangeScreenCommand(sceneRegister, sceneManager, "ladder-game"), true));
+        "Swap to Board View", new ChangeScreenCommand(
+            sceneRegister, sceneManager, "ladder-game"),
+        true));
     menuOptions.add(new MenuOption("Settings", new PrintLineCommand("Settings"), true));
     menuOptions.add(new MenuOption("Players", new PrintLineCommand("Players"), true));
     menuOptions.add(new MenuOption("Exit", new PrintLineCommand("Exit"), true));
@@ -52,20 +60,50 @@ public class ViewFactory {
     return menu.asParent();
   }
 
-  public Parent buildAddPlayerView(GameSession gameSession, SceneManager sceneManager, PlayerDaoImpl playerDao) {
+  /**
+   * Builds the add player view.
+   *
+   * @param gameSession  the current game session
+   * @param sceneManager the scene manager
+   * @param playerDao    the player DAO implementation
+   * @return the root node of the add player view
+   */
+  public Parent buildAddPlayerView(
+      GameSession gameSession,
+      SceneManager sceneManager,
+      PlayerDaoImpl playerDao) {
     logger.info("Building Add Player view");
-    AddPlayerViewController addPlayerViewController = new AddPlayerViewController(gameSession, sceneManager, playerDao);
+    AddPlayerViewController addPlayerViewController = new AddPlayerViewController(
+        gameSession, sceneManager, playerDao);
     AddPlayerView view = new AddPlayerView(addPlayerViewController);
     return view.getRoot();
   }
 
+  /**
+   * Builds the game builder view.
+   *
+   * @param gameSession   the current game session
+   * @param sceneManager  the scene manager
+   * @param sceneRegister the scene register
+   * @return the root node of the game builder view
+   */
   public Parent buildGameBuilderView(
       GameSession gameSession, SceneManager sceneManager, SceneRegister sceneRegister) {
-    GameBuilderView gbView = new GameBuilderView(new GameBuilderController(gameSession, sceneRegister, sceneManager));
+    GameBuilderView gbView = new GameBuilderView(
+        new GameBuilderController(gameSession, sceneRegister, sceneManager));
     return gbView.getRoot();
   }
 
-  public Parent buildPlayerListView(PlayerDaoImpl playerDao, Map<String, StatsManager<?>> statsManagers) {
+  /**
+   * Builds the player list view.
+   *
+   * @param playerDao     the player DAO implementation
+   * @param statsManagers the map of stats managers
+   * @return the root node of the player list view
+   */
+  public Parent buildPlayerListView(
+      PlayerDaoImpl playerDao,
+      Map<String, StatsManager<?>> statsManagers) {
     PlayerListViewController controller = new PlayerListViewController(playerDao, statsManagers);
     PlayerListView view = new PlayerListView(controller);
     return view.getRoot();
