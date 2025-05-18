892fd2afd91e4082b34c6ade722155580af98e12
Ink-Song <sunruh@proton.me>
2025-05-18 17:00:42 +0200

    Modified gameBuilderView to temporarily remove rules column.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/GameBuilderView.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/view/GameBuilderView.java
@@ -78,7 +78,9 @@ public class GameBuilderView {
 
   private void configureView() {
     root.setMaxSize(width, height);
-    float columnWidth = width / 3;
+    //float columnWidth = width / 3;
+    //Temporarily removed rules column.
+    float columnWidth = width/2;
     boardColumn.setMaxWidth(columnWidth);
     rulesColumn.setMaxWidth(columnWidth);
     playerColumn.setMaxWidth(columnWidth);
@@ -94,7 +96,9 @@ public class GameBuilderView {
         gameMenu, boardHeader, boardMenu, startGameButton);
     rulesColumn.getChildren().add(rulesTitle);
 
-    layout.getChildren().addAll(playerColumn, boardColumn, rulesColumn);
+    //layout.getChildren().addAll(playerColumn, boardColumn, rulesColumn);
+    // Temporarily removed rules column.
+    layout.getChildren().addAll(playerColumn, boardColumn);
     root.getChildren().add(background);
     root.getChildren().add(layout);
   }
