bdbaf4370889cc6ac05f25f8eaa485dcdf7506fa
Dennis Moe <dennismoe0@gmail.com>
2025-05-21 16:42:05 +0200

    Changed it to GameEventHandler, why not.

--- boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/service/GameEventHandler.java
+++ boardgames/src/main/java/no/ntnu/idatx2003/exam2025/boardgames/service/GameEventHandler.java
@@ -3,11 +3,11 @@ package no.ntnu.idatx2003.exam2025.boardgames.service;
 import no.ntnu.idatx2003.exam2025.boardgames.model.boardgame.BoardGame;
 
 /**
- * Interface for handling the end of a board game.
+ * Interface for handling the event in a board game.
  * This could definitely also be an extension of a GameEventHandler, but we dont
  * have the time.
  */
-public interface GameEndHandler {
+public interface GameEventHandler {
 
   /**
    * Handles the end of the game for the given BoardGame.
